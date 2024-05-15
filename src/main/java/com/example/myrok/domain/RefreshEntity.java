18789352603e96a4d414e2b3497cc7fdd9598a90
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-05-16 04:06:58 +0900

    feat: Refresh 토큰 서버측 저장

---
+++ src/main/java/com/example/myrok/domain/RefreshEntity.java
@@ -0,0 +1,22 @@
+package com.example.myrok.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Getter
+@Setter
+public class RefreshEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String refresh;
+    private String expiration;
+}
