ac5721d4708edaed37914e7de45d38a697bac41e
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-05-16 12:40:53 +0900

    feat: 메소드 추가

--- src/main/java/com/example/myrok/repository/UserRepository.java
+++ src/main/java/com/example/myrok/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.security.core.userdetails.User;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
 }
