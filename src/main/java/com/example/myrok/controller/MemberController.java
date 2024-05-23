c1a4529d1426c188db4e3e51ef5bb2a1574f8b85
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-05-23 12:47:08 +0900

    feat: 본인 정보 조회

--- src/main/java/com/example/myrok/controller/MemberController.java
+++ src/main/java/com/example/myrok/controller/MemberController.java
@@ -2,7 +2,10 @@ package com.example.myrok.controller;
 
 
 import com.example.myrok.domain.Member;
+<<<<<<< HEAD
 import com.example.myrok.dto.MemberProjectResponse;
+=======
+>>>>>>> b37d6d3 (feat: 본인 정보 조회)
 import com.example.myrok.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +18,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+<<<<<<< HEAD
+=======
+@RequestMapping("/api/me")
+>>>>>>> b37d6d3 (feat: 본인 정보 조회)
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
+<<<<<<< HEAD
     @GetMapping("/api/me")
+=======
+    @GetMapping
+>>>>>>> b37d6d3 (feat: 본인 정보 조회)
     public ResponseEntity<Member> getMyInformation(@PathVariable String email) {
 
         return ResponseEntity.ok().body(memberService.getMemberInformation(email));
     }
 
+<<<<<<< HEAD
     @GetMapping("myrok/me/project")
     public ResponseEntity<MemberProjectResponse> getParticipatedMemberProject(@PathVariable String email) {
 
         return ResponseEntity.ok(memberService.getParticipatedMemberProject(email));
     }
+=======
+
+>>>>>>> b37d6d3 (feat: 본인 정보 조회)
 }
